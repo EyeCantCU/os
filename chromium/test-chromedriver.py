3c9209049c5dc44fac39be21e1a1bee95e1591f2
RJ Sampson <rj.sampson@chainguard.dev>
2024-02-29 18:10:31 -0700

    chore(chromium): Add ChromeDriver test
    Ensures ChromeDriver works
    
    Signed-off-by: RJ Sampson <rj.sampson@chainguard.dev>

---
+++ chromium/test-chromedriver.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get('http://www.google.com/');
+time.sleep(5)
+search_box = driver.find_element_by_name('q')
+search_box.send_keys('ChromeDriver')
+search_box.submit()
+driver.quit()
