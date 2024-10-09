b47b148a6cb5e0602a07d957f0f9069e6394800b
John Osborne <johnfosborneiii@gmail.com>
2024-10-09 12:12:39 -0500

    Update request-4334.yaml (#8368)

--- request-4334/entrypoint.sh
+++ request-4334/entrypoint.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-# Overwrite /etc/apk/repositories with the Danske Artifactory Mirrors
-cat <<EOF > /etc/apk/repositories
-https://artifactory.danskenet.net/artifactory/remote-alpine-cgr-extras
-https://artifactory.danskenet.net/artifactory/remote-alpine-wolfi-packages
-EOF
+# Append /etc/apk/repositories with the Danske Artifactory Mirrors
+apk --repository="/etc/apk/danske-mirrors" update
 
 # Execute any additional commands or scripts if needed
 exec "$@"
