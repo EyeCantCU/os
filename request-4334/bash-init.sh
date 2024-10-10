3cb0d79dea31d7d793d2b88a12f77319c59bc711
John Osborne <johnfosborneiii@gmail.com>
2024-10-10 18:16:40 -0500

    Update request-4334.yaml (#8464)

---
+++ request-4334/bash-init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Check if we are in the Chainguard Build Ecosystem
+if [[ -n "${targets.contextdir}" ]]; then
+    "Running inside the Chainguard Build Ecosystem"
+else
+    echo "https://artifactory.danskenet.net/artifactory/remote-alpine-cgr-extras" > /etc/apk/repositories
+    echo "https://artifactory.danskenet.net/artifactory/remote-alpine-wolfi-packages" >> /etc/apk/repositories
+fi
--- request-4334/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Append /etc/apk/repositories with the Danske Artifactory Mirrors
-apk --repository="/etc/apk/danske-mirrors" update
-
-# Execute any additional commands or scripts if needed
-exec "$@"
