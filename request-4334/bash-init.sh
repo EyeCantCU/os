d9f43e814ce7081bc1c3ed86d0b6d5136daf1378
John Osborne <johnfosborneiii@gmail.com>
2024-10-17 14:24:14 -0500

    Mv update ca certs to init but not in test (#8605)
    * mv update-ca-certs to init scripts, add downward API env var for init scripts
    
    * Update bash-init.sh to run update-ca-certificates when not in the Chainguard Build Ecosystem

--- request-4334/bash-init.sh
+++ request-4334/bash-init.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 # Check if we are in the Chainguard Build Ecosystem
-if [[ -n "${targets:-}" && -n "${targets.contextdir:-}" ]]; then
-    "Running inside the Chainguard Build Ecosystem"
+if [[ -n "${targets:-}" && -n "${targets.contextdir:-}" || "${TEST_PIPELINE}" == "true" ]]; then
+    echo "Running inside the Chainguard Build Ecosystem"
 else
     echo "https://artifactory.danskenet.net/artifactory/remote-alpine-cgr-extras" > /etc/apk/repositories
     echo "https://artifactory.danskenet.net/artifactory/remote-alpine-wolfi-packages" >> /etc/apk/repositories
+
+    # Update-ca-certificates will cause an infinite bash loop if you force bash upon it when it thinks its invoking regular /bin/sh so making a temp symb link to busybox
+    mv /bin/sh /bin/sh.bak
+    ln -sf /bin/busybox /bin/sh
+    update-ca-certificates
+    rm /bin/sh
+    mv /bin/sh.bak /bin/sh
 fi
