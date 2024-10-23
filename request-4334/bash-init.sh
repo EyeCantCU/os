8682b4b835145b576f633a6469b407cfa3abf3c4
John Osborne <johnfosborneiii@gmail.com>
2024-10-23 11:08:01 -0400

    Mv certs update build (#8863)
    * Update bash-init.sh
    
    * Update request-4334.yaml
    
    * Revert "Update request-4334.yaml"
    
    This reverts commit 05a1a4624851a8b3aaebcfc85dd1e40530ca0a66.
    
    * Update request-4334.yaml
    
    * Update danske-custom-certs.crt
    
    * Update request-4334.yaml
    
    * Update request-4334.yaml
    
    * Update request-4334.yaml
    
    * Update request-4334.yaml
    
    * Update request-4334.yaml
    
    * Update request-4334.yaml
    
    * Update request-4334.yaml
    
    * Update request-4334.yaml
    
    * Update request-4334.yaml
    
    * Update request-4334.yaml
    
    * Update request-4334.yaml

--- request-4334/bash-init.sh
+++ request-4334/bash-init.sh
@@ -6,11 +6,4 @@ if [[ -n "${targets:-}" && -n "${targets.contextdir:-}" || "${TEST_PIPELINE}" ==
 else
     echo "https://artifactory.danskenet.net/artifactory/remote-alpine-cgr-extras" > /etc/apk/repositories
     echo "https://artifactory.danskenet.net/artifactory/remote-alpine-wolfi-packages" >> /etc/apk/repositories
-
-    # Update-ca-certificates will cause an infinite bash loop if you force bash upon it when it thinks its invoking regular /bin/sh so making a temp symb link to busybox
-    mv /bin/sh /bin/sh.bak
-    ln -sf /bin/busybox /bin/sh
-    update-ca-certificates >/dev/null 2>&1
-    rm /bin/sh
-    mv /bin/sh.bak /bin/sh
 fi
