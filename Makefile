EyeCantCU/os

commit 6e973167be69333a4d528bf9b9f4b3588428fb9b

Add support to download ovmf from qemu-system apk (#39)
Move downloading package function to 'tools/fetch-pkg'. It supports
'wolfi/<pkg>' or 'chainguard-private/<pkg>'.

Use that to "grab-pkg-artifact" for kernel or ovmf.

Co-authored-by: Andres Rodriguez <andreserl@gmail.com>

Makefile
@@ -151,16 +151,17 @@ run-builder: $(BUILDER_DEPS)
 .PHONY: builder
 builder: $(BUILDER_DEPS)
 
-builder/kernel-%: $(TOOLS_D)/fetch-linux-kernel
+builder/kernel-%: $(TOOLS_D)/grab-pkg-artifact
 	@mkdir -p $(dir $@)
-	@$(call withauth,$(TOOLS_D)/fetch-linux-kernel --arch="$(ARCH)" --output="$@")
+	@$(call withauth,$(TOOLS_D)/grab-pkg-artifact "--arch=$*" kernel $@)
 
 builder/initrd-%: mkvm.yaml
 	@mkdir -p $(dir $@)
 	@$(call apko_build,cpio,$<,$@)
 
-builder/ovmf-%.fd:
-	$(TOOLS_D)/make-ovmf $@ $*
+builder/ovmf-%.fd: $(TOOLS_D)/grab-pkg-artifact
+	@mkdir -p $(dir $@)
+	$(TOOLS_D)/grab-pkg-artifact "--arch=$*" ovmf $@
 
 %.vmdk: %.raw
 	qemu-img convert -O vmdk -o subformat=streamOptimized $< $@

tools/fetch-linux-kernel
@@ -1,116 +1,0 @@
-#!/bin/sh
-
-set -e
-
-usage() {
-	echo "
-$0 [-a|--arch x86_64|aarch64] [-v|--version x.y.z-r*] [-l|--list]
-
- -a|--arch [x86_64|aarch64]	Linux kernel architecture; defaults to your current system architecture per 'uname -m'
- -l|--list			List Linux kernel versions available
- -v|--version [x.y.z-r*]	Linux kernel version
-
-"
-}
-
-error() {
-	echo "ERROR:" "$@" 1>&2
-	exit 1
-}
-
-# Default to current system architecture
-arch=$(uname -m)
-dir=$(dirname "$0")
-output=""
-while [ ! -z "$1" ]; do
-	case "$1" in
-        --output=*) output=${1#*=}; shift 1;;
-		-o|--output) output=$2; shift 2;;
-        --arch=*) arch=${1#*=}; shift 1;;
-		-a|--arch)
-			case "$2" in
-				x86_64|aarch64)
-					arch="$2"
-				;;
-				*)
-					error "Arch must be x86_64 or aarch64"
-				;;
-			esac
-			shift 2
-		;;
-		-l|--list)
-			list=1
-			shift 1
-		;;
-		-v|--version)
-			[ -z "${2}" ] && \
-				{ echo "Version must be specified"; usage; exit 1; }
-			ver="$2"
-			shift 2
-		;;
-		-h|--help)
-			usage
-			exit 0
-		;;
-		*)
-			usage
-			exit 12
-		;;
-	esac
-done
-
-# take HTTP_AUTH="basic:apk.cgr.dev:user:<tok>"
-case "$HTTP_AUTH" in
-	basic:apk.cgr.dev:user:*)
-        tok=${HTTP_AUTH#basic:apk.cgr.dev:}
-        curlauth="--user $tok"
-        ;;
-	*) tok=$(chainctl auth token --audience=apk.cgr.dev) ||
-		error "failed to get token for apk.cgr.dev"
-        curlauth="--user user:$tok"
-        ;;
-esac
-
-apkrepo="https://apk.cgr.dev/chainguard-private/$arch"
-apkindex="$apkrepo/APKINDEX.tar.gz"
-
-# List kernel versions available and exit
-if [ "$list" = "1" ]; then
-	curl -L --silent $curlauth --output - "$apkindex" |
-		zcat |
-		grep -a -A1 "^P:linux" |
-		grep "^V:" |
-		sort -V |
-		sed -e "s/^V://"
-	exit 0
-fi
-
-# Fetch the package list, grep for the linux package, and find the latest version
-if [ -z "$ver" ]; then
-	ver=$(curl -L --silent $curlauth --output - "$apkindex" |
-		zcat |
-		grep -a -A1 "^P:linux" |
-		grep "^V:" |
-		sort -V |
-		tail -n1 | sed -e "s/^V://")
-    [ -n "$ver" ] || error "failed to get linux version from $apkindex"
-fi
-kernel="boot/vmlinuz-$arch-$ver"
-
-apk="linux-$ver.apk"
-if [ -z "$output" ]; then
-    output=$dir/$(basename "$kernel")
-fi
-tmpf="$output.tmp.$$"
-# Fetch the linux apk package, extract a vmlinuz kernel from a package and write it to the local directory
-echo "fetching $kernel from $apk to $output" 1>&2
-curl -L --silent $curlauth "$apkrepo/$apk" |
-	tar -xzO "$kernel" > "$tmpf" 2>/dev/null || {
-		rm -f "$tmpf"
-		error "failed extracting $kernel from $apkrepo/$apk"
-	}
-
-if [ ! -s "$tmpf" ]; then
-	error "empty file created - likely didn't find $kernel in $apkrepo/$apk"
-fi
-mv "$tmpf" "$output" || error "failed renaming $tmpf to $output"

tools/fetch-pkg
@@ -1,0 +1,130 @@
+#!/bin/sh
+# shellcheck disable=SC2015,SC2039,SC2166,SC3043
+# vi: tabstop=4 expandtab
+
+Usage() {
+    cat <<EOF
+${0##*/} [-a|--arch x86_64|aarch64] [-v|--version x.y.z-r*] [-l|--list] op
+
+ -p|--package [linux|qemu-system-<arch>]    Package to download; defaults to 'linux', 'qemu-system-x86_64' or 'qemu-system-aarch64'
+ -a|--arch [x86_64|aarch64]                 Linux kernel architecture; defaults to your current system architecture per 'uname -m'
+ -l|--list                                  List Linux kernel versions available
+ -v|--version [x.y.z-r*]                    Linux kernel version
+EOF
+}
+
+fail() {
+	echo "ERROR:" "$@" 1>&2
+	exit 1
+}
+
+get_curl_auth() {
+	# take HTTP_AUTH="basic:apk.cgr.dev:user:<tok>"
+	case "$HTTP_AUTH" in
+		basic:apk.cgr.dev:user:*)
+	        tok=${HTTP_AUTH#basic:apk.cgr.dev:}
+	        curlauth="--user $tok"
+	        ;;
+		*) tok=$(chainctl auth token --audience=apk.cgr.dev) ||
+			fail "failed to get token for apk.cgr.dev"
+	        curlauth="--user user:$tok"
+	        ;;
+	esac
+	echo "$curlauth"
+}
+
+# Default to current system architecture
+arch=$(uname -m)
+package=""
+output=""
+list=false
+while [ -n "$1" ]; do
+	case "$1" in
+        --output=*) output=${1#*=}; shift 1;;
+		-o|--output) output=$2; shift 2;;
+        --arch=*) arch=${1#*=}; shift 1;;
+		-a|--arch) arch="$2"; shift 2;;
+		-l|--list) list=true; shift 1;;
+		-v|--version)
+			[ -n "$2" ] || { Usage echo "Version must be specified"; exit 1; } 1>&2
+			ver="$2"
+			shift 2
+		;;
+		-h|--help)
+			Usage
+			exit 0
+		;;
+        -*) fail "Unexpected flag '$1'";;
+		*)
+            [ -z "$package" ] ||
+                fail "confused by argument '$1'. expected only one arg (package)"
+            package="$1"
+            shift
+		;;
+	esac
+done
+
+case "$arch" in
+    x86_64|aarch64) :;;
+    *) fail "unknown arch $arch - must be x86_64 or aarch64";;
+esac
+
+case "$package" in
+    chainguard-private/*)
+        apkrepo="https://apk.cgr.dev/chainguard-private/$arch"
+        package=${package##*/}
+        curlauth=$(get_curl_auth) || exit
+        ;;
+    wolfi/*)
+	    apkrepo="https://packages.wolfi.dev/os/$arch";
+        package=${package##*/}
+        curlauth=""
+        ;;
+    */*) fail "unknown name for repo in $package (package has a /)";;
+    *)
+        apkrepo="https://apk.cgr.dev/chainguard-private/$arch"
+        curlauth=$(get_curl_auth) || exit
+        ;;
+esac
+
+apkindex="$apkrepo/APKINDEX.tar.gz"
+
+if [ -z "$version" ]; then
+    # shellcheck disable=SC2086
+    vlist=$(curl -L --silent $curlauth --output - "$apkindex" |
+		    zcat |
+		    grep -a -A1 "^P:$package" |
+		    grep "^V:" |
+		    sort -V |
+		    sed -e "s/^V://"
+    )
+    [ -n "$vlist" ] || fail "unable to get version list for '$package' from $apkindex"
+fi
+
+# List versions available and exit
+if [ "$list" = "true" ]; then
+    echo "$vlist"
+	exit 0
+fi
+
+if [ -z "$ver" ]; then
+    ver=$(echo "$vlist" | tail -n1)
+    [ -n "$ver" ] || fail "failed to get $package version from $apkindex"
+fi
+
+apk="$package-$ver.apk"
+if [ -z "$output" ]; then
+    output="$apk"
+fi
+tmpf="$output.tmp.$$"
+
+# Fetch the apk package, extract a file from a package and write it to the local directory
+echo "fetching $apk to $output" 1>&2
+# shellcheck disable=SC2086
+curl -L --silent $curlauth "$apkrepo/$apk" > "$tmpf" ||
+    fail "curl failed on $apkrepo/$apk"
+if [ ! -s "$tmpf" ]; then
+	fail "empty file created from $apkrepo/$apk"
+fi
+mv "$tmpf" "$output" || fail "failed renaming $tmpf to $output"
+echo "wrote $apk to $output" 1>&2

tools/grab-pkg-artifact
@@ -1,0 +1,131 @@
+#!/bin/sh
+# shellcheck disable=SC2015,SC2166,SC3043
+TEMP_D=""
+VERBOSITY=0
+
+Usage() {
+    cat <<EOF
+Usage: ${0##*/} [options] thing output
+
+  Get 'thing' from a wolfi or chainguard repo.
+
+  Currently supported 'thing' is:
+     ovmf
+     kernel
+
+  options:
+
+    -v | --verbose        be more verbose
+    -a | --arch   ARCH    provide arch (default 'uname -m')
+
+  Environment:
+
+     HTTP_AUTH - can provide auth for curl
+EOF
+}
+
+cleanup() {
+    [ -n "$TEMP_D" ] || return 0
+    rm -Rf "$TEMP_D"
+}
+
+stderr() { echo "$@" 1>&2; }
+fail() { [ $# -eq 0 ] || stderr "$@"; exit 1; }
+
+main() {
+    local short_opts="a:hv"
+    local long_opts="arch:,help,name:,verbose"
+    local getopt_out=""
+    getopt_out=$(getopt --name "${0##*/}" \
+        --options "${short_opts}" --long "${long_opts}" -- "$@") &&
+        eval set -- "${getopt_out}" ||
+        { Usage 1>&2; return 1; }
+
+    local cur="" next="" thing="" out="" arch=""
+    while [ $# -ne 0 ]; do
+        { cur="$1"; next="$2"; }
+        case "$cur" in
+            -h|--help) Usage ; exit 0;;
+            -a|--arch) arch=$next; shift;;
+            -v|--verbose) VERBOSITY=$((VERBOSITY+1));;
+            --) shift; break;;
+        esac
+        shift;
+    done
+
+    if [ -z "$arch" ]; then
+        arch=$(uname -m) || fail "uname -m failed"
+    fi
+
+    if [ $# -ne 2 ]; then
+        Usage 1>&2
+        stderr "Got $# args, expected 2"
+        return 1
+    fi
+
+    case "$arch" in
+        x86_64|aarch64) :;;
+        *) fail "bad arch $arch";;
+    esac
+
+    thing="$1"
+    outfile="$2"
+
+    if ! command -v fetch-pkg >/dev/null 2>&1; then
+        PATH="${0%/*}:$PATH"
+        command -v fetch-pkg >/dev/null 2>&1 || fail "no fetch-pkg in path"
+    fi
+
+    case "$thing" in
+        kernel)
+            package="chainguard-private/linux"
+            pkgarch="$arch"
+            ;;
+        ovmf)
+            case "$arch" in
+                x86_64|aarch64)
+                    package="wolfi/qemu-system-$arch"
+                    ;;
+                *) fail "unknown arch $arch"
+            esac
+            ;;
+    esac
+
+    local fullver="" out="" extract_file=""
+    out=$(fetch-pkg --list "$package") || fail "failed list versions of $package"
+    # fullver is like 6.13-r1
+    fullver=$(echo "$out" | tail -n 1)
+
+    case "$thing" in
+        kernel) extract_file="boot/vmlinuz-$arch-$fullver";;
+        ovmf) extract_file="usr/share/qemu/edk2-$arch-code.fd";;
+    esac
+
+    TEMP_D=$(mktemp -d "${TMPDIR:-/tmp}/${0##*/}.XXXXXX") ||
+        { stderr "failed to create tempdir"; return 1; }
+    trap cleanup EXIT
+
+    local apk="${TEMP_D}/${package#*/}-$fullver.apk"
+    local tmpf="$outfile.$$"
+
+    fetch-pkg "--output=$apk" "$package" ||
+        fail "failed to download $package at $fullver"
+
+    tmpf="$outfile.$$"
+    tar -xzOf "$apk" "$extract_file" > "$tmpf" 2>/dev/null || {
+        rm -f "$tmpf"
+        fail "failed extracting $extract_file from $package"
+    }
+
+    if [ ! -s "$tmpf" ]; then
+        rm -f "$tmpf"
+        fail "$package seemed to have no file $extract_file"
+    fi
+
+    mv "$tmpf" "$outfile" || fail "failed renaming $tmpf to $outfile"
+    echo "pulled $thing from $package=$fullver $extract_file to $outfile"
+
+    return 0
+}
+
+main "$@"

commit 4e00e50b69987ee017b0f4bf66822113c72d85be

Fix booting with ovmf/edk2 file from qemu (#45)
At very least, on Ubuntu 24.04 qemu (8.2.2+ds-0ubuntu1.5) the commit
6e973167be69333 broke 'run-disk-' and 'debug-disk-' targets. It would
fail with error:

    qemu: could not load PC BIOS 'builder/ovmf-x86_64.fd'

Instead of '-bios', use the -if=pflash option.

This does create a vm without space to write vars, but that is fine for
now (and no different than before).

Makefile
@@ -70,9 +70,10 @@ raw_disk_args = \
  -blockdev driver=raw,node-name=$(notdir $1),file.driver=file,file.filename=$(1) \
  -device virtio-blk-pci,drive=$(notdir $1),serial=$(2),discard=true
 
+
 define qemu-disk
 	$(QEMU_CMD) \
-	  -bios builder/ovmf-$(ARCH).fd \
+	  -drive "if=pflash,format=raw,file=builder/ovmf-$(ARCH).fd,readonly=on" \
 	  $(call raw_disk_args,$1,boot-disk)
 endef
 