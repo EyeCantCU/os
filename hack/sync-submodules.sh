e3f48d77e80033ce5aced87fa1742497faf61a69
Jason Hall <jason@chainguard.dev>
2025-03-25 15:09:59 -0400

    move to a bash script for testing
    Signed-off-by: Jason Hall <jason@chainguard.dev>

---
+++ hack/sync-submodules.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+git submodule
+
+# If there are any changes, proceed
+if [[ -n $(git status --porcelain) ]]; then
+    git checkout -b sync-submodules
+    git commit -asm "update submodules"
+    git push origin sync-submodules --force
+
+    # If PR exists for this branch, update it
+    if gh pr list --state open --base main --head sync-submodules | grep -q sync-submodules; then
+        gh pr update --title "update submodules" --body "update submodules"
+    else
+        gh pr create --title "update submodules" --body "update submodules" --base main
+    fi
+else
+    echo "No changes to submodules, nothing to do"
+fi
+
+git checkout -
