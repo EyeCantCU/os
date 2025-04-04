e4ad7aeb17b895c0c045cabaa67abf2f7aba67f9
Rob Best <robertbest89@gmail.com>
2025-04-04 14:39:36 +0100

    request-4334: add private APK repository (#20200)
    This is an Artifactory proxy mirror of Danske's private APK feed.
    
    Signed-off-by: Rob Best <rob.best@chainguard.dev>

--- request-4334/bash-init.sh
+++ request-4334/bash-init.sh
@@ -4,6 +4,7 @@
 if [[ -n "${targets:-}" && -n "${targets.contextdir:-}" || "${TEST_PIPELINE}" == "true" ]]; then
     echo "Running inside the Chainguard Build Ecosystem"
 else
-    echo "https://artifactory.danskenet.net/artifactory/remote-alpine-cgr-extras" > /etc/apk/repositories
+    echo "https://artifactory.danskenet.net/artifactory/remote-alpine-cgr-danske" > /etc/apk/repositories
+    echo "https://artifactory.danskenet.net/artifactory/remote-alpine-cgr-extras" >> /etc/apk/repositories
     echo "https://artifactory.danskenet.net/artifactory/remote-alpine-wolfi-packages" >> /etc/apk/repositories
 fi
