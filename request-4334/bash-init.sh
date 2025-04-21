4d33f60c384197922078e639566695cdadf8a8bb
kranurag7 <81210977+kranurag7@users.noreply.github.com>
2025-04-22 01:04:18 +0530

    request-4334: don't error out with nonroot user (#22017)
    customer is adding this package to image but they want to error out when
    the image is running with nonroot user.
    
    we're doing a id -u check here so if we run the nonroot user we don't
    see any error.
    
    Signed-off-by: kranurag7 <81210977+kranurag7@users.noreply.github.com>

--- request-4334/bash-init.sh
+++ request-4334/bash-init.sh
@@ -4,7 +4,9 @@
 if [[ -n "${targets:-}" && -n "${targets.contextdir:-}" || "${TEST_PIPELINE}" == "true" ]]; then
     echo "Running inside the Chainguard Build Ecosystem"
 else
-    echo "https://artifactory.danskenet.net/artifactory/remote-alpine-cgr-danske" > /etc/apk/repositories
-    echo "https://artifactory.danskenet.net/artifactory/remote-alpine-cgr-extras" >> /etc/apk/repositories
-    echo "https://artifactory.danskenet.net/artifactory/remote-alpine-wolfi-packages" >> /etc/apk/repositories
+    if [[ "$(id -u)" -eq 0 ]]; then
+        echo "https://artifactory.danskenet.net/artifactory/remote-alpine-cgr-danske" > /etc/apk/repositories
+        echo "https://artifactory.danskenet.net/artifactory/remote-alpine-cgr-extras" >> /etc/apk/repositories
+        echo "https://artifactory.danskenet.net/artifactory/remote-alpine-wolfi-packages" >> /etc/apk/repositories
+    fi
 fi
