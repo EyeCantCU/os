1d4a18f89823d7695cd3f144c1a1ed6edf502eb5
Scott Moser <smoser@brickies.net>
2025-04-22 13:39:46 -0400

    qemu - add run-remote for exercising test.

--- pkg/runner/qemu/cmd/ssh.go
+++ pkg/runner/qemu/cmd/ssh.go
@@ -42,3 +42,45 @@ func sshCmd() *cobra.Command {
 
 	return cmd
 }
+
+func runRemoteCmd() *cobra.Command {
+	var vmdir, sshKeyFile, user, localFilePath string
+
+	cmd := &cobra.Command{
+		Use:   "run-remote",
+		Short: "run a binary on an Azure VM by tag",
+		Args:  cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			ctx := cmd.Context()
+
+			vmdir = args[0]
+			cmdArgs := args[1:]
+
+			sshAddr, sshPort, err := getSSHPortAddr(ctx, filepath.Join(vmdir, "qmp.sock"))
+			if err != nil {
+				log.Fatalf("Failed to get ssh port from %s: %v\n", vmdir, err)
+			}
+			hostPort := fmt.Sprintf("%s:%d", sshAddr, sshPort)
+
+			remoteFile := filepath.Join("/tmp", filepath.Base(localFilePath))
+
+			err = sshutils.ShoveBinaryFile(ctx, hostPort, sshKeyFile, user, localFilePath, remoteFile)
+			if err != nil {
+				log.Fatalf("failed to move %s to remote host: %v", localFilePath, err)
+			}
+
+			remotecmd := append([]string{"exec", remoteFile}, cmdArgs...)
+
+			if err := sshutils.SSHToInstance(ctx, hostPort, sshKeyFile, user, remotecmd); err != nil {
+				log.Fatalf("failed to run %s on remote host: %v", filepath.Base(localFilePath), err)
+			}
+		},
+	}
+
+	cmd.Flags().StringVar(&localFilePath, "file", "", "File to run remotely (required)")
+	cmd.Flags().StringVarP(&user, "user", "u", "linky", "USER")
+	cmd.Flags().StringVarP(&sshKeyFile, "private-key", "i", "", "id_ed25519")
+	cmd.MarkFlagRequired("file")
+
+	return cmd
+}
