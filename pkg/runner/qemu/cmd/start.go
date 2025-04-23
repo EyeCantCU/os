82108892eb0e8fac65d938cdf431ad484d02fd7b
Scott Moser <smoser@brickies.net>
2025-04-23 13:38:22 -0400

    qemu start - correctly kill qemu process when start is killed.
    Prior to this, a the qemu-system child process would be killed on
    a ctrl-c, but not on a TERM.
    
    The changes here send the signal onto qemu-system and wait.
    
    Also there are cleanups in the way the Start function works
    that hopefully make it more readable.

--- pkg/runner/qemu/cmd/start.go
+++ pkg/runner/qemu/cmd/start.go
@@ -6,15 +6,18 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"os/signal"
 	"path/filepath"
 	"runtime"
 	"strings"
+	"syscall"
 	"time"
 
 	"chainguard.dev/apko/pkg/build/types"
 	"chainguard.dev/wolfi-vm/vm-test/pkg/runner/qemu/util"
 	"chainguard.dev/wolfi-vm/vm-test/pkg/runner/qemu/util/qmp"
 	"github.com/spf13/cobra"
+	"golang.org/x/sys/unix"
 )
 
 func startCmd() *cobra.Command {
@@ -37,7 +40,7 @@ func startCmd() *cobra.Command {
 			}
 			err = Start(cmd.Context(), args[0], pkdata, types.ParseArchitecture(arch))
 			if err != nil {
-				log.Fatalf("Error starting vm: %v\n", err)
+				log.Fatal(err)
 			}
 		},
 	}
@@ -46,6 +49,14 @@ func startCmd() *cobra.Command {
 	cmd.Flags().StringVar(&arch, "arch", runtime.GOARCH, "ARCH")
 
 	return cmd
+}
+
+func sigStr(sig os.Signal) string {
+	sigstr := sig.String()
+	if s, ok := sig.(syscall.Signal); ok {
+		sigstr = unix.SignalName(s)
+	}
+	return sigstr
 }
 
 func getSSHPortAddr(ctx context.Context, sockPath string) (string, int, error) {
@@ -74,8 +85,8 @@ func getSSHPortAddr(ctx context.Context, sockPath string) (string, int, error) {
 }
 
 func Start(ctx context.Context, dir, pkdata string, arch types.Architecture) error {
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
+	ctx, cancel := context.WithCancelCause(ctx)
+	defer cancel(nil)
 
 	monitorSock := filepath.Join(dir, "monitor.sock")
 	if err := os.Remove(monitorSock); err != nil && !os.IsNotExist(err) {
@@ -87,38 +98,55 @@ func Start(ctx context.Context, dir, pkdata string, arch types.Architecture) err
 		[]string{"-smbios", "type=1,product=cgr.dev/qemu/v1",
 			"-smbios", "type=11,value=cgr.dev/qemu/v1/ssh-pubkey=" + pkdata}...)
 
-	var sshAddr string
-	var sshPort int
-	var cmd *exec.Cmd
+	sigs := make(chan os.Signal, 1)
+	var sigReceived os.Signal
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
-	cmdChan := make(chan error)
-	sshChan := make(chan error)
-
-	go func() {
-		cmd = exec.CommandContext(ctx, args[0], args[1:]...)
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		cmd.Dir = dir
-		log.Printf("Starting qemu in %s: %v\n", dir, cmd.Args)
-		cmdChan <- cmd.Run()
-	}()
-
-	go func() {
-		var err error
-
-		sshAddr, sshPort, err = getSSHPortAddr(ctx, filepath.Join(dir, "qmp.sock"))
-		sshChan <- err
-	}()
-
-	for {
-		select {
-		case err := <-sshChan:
-			if err != nil {
-				return err
-			}
-			log.Printf("ssh available on %s:%d\n", sshAddr, sshPort)
-		case err := <-cmdChan:
-			return err
+	cmd := exec.CommandContext(ctx, args[0], args[1:]...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Dir = dir
+	// kill with SIGTERM rather than SIGKILL
+	cmd.Cancel = func() error {
+		if cmd.Process == nil {
+			log.Printf("context canceled but pid cmd.Process is nil")
+			return nil
 		}
+		log.Printf("sending %s to %s pid %d", sigStr(sigReceived), args[0], cmd.Process.Pid)
+		cmd.Process.Signal(sigReceived)
+		_, err := cmd.Process.Wait()
+		return err
 	}
+
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+
+	go func() {
+		select {
+		case sigReceived = <-sigs:
+			log.Printf("%s received. shutting down", sigStr(sigReceived))
+			cancel(fmt.Errorf("%s received", sigStr(sigReceived)))
+		case <-ctx.Done():
+		}
+	}()
+
+	go func() {
+		qmpSock := filepath.Join(dir, "qmp.sock")
+		sshAddr, sshPort, err := getSSHPortAddr(ctx, qmpSock)
+		if err != nil {
+			if ctx.Err() != nil {
+				cancel(fmt.Errorf("failed to get ssh connection info: %v", err))
+			}
+			return
+		}
+		log.Printf("%s is pid %d. qmp-socket in %s\n", args[0], cmd.Process.Pid, qmpSock)
+		log.Printf("ssh available on %s:%d\n", sshAddr, sshPort)
+	}()
+
+	err := cmd.Wait()
+	if ctx.Err() != nil {
+		return ctx.Err()
+	}
+	return err
 }
