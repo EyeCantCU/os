96fcba8c1868bfa0a9e74ec95e7d4b72b315b522
Amelia Crate <amelia.crate@chainguard.dev>
2025-05-28 13:39:47 -0700

    aws: wait for console logs in test-wolfi-vm (#373)

--- vms-test/pkg/runner/aws/cmd/console-log.go
+++ vms-test/pkg/runner/aws/cmd/console-log.go
@@ -4,8 +4,11 @@ import (
 	"encoding/base64"
 	"fmt"
 	"log"
+	"strings"
+	"time"
 
-	"chainguard.dev/wolfi-vm/vm-test/pkg/runner/aws/util"
+	"chainguard.dev/wolfi-vm/vm-test/pkg/internal/utils"
+	awsutil "chainguard.dev/wolfi-vm/vm-test/pkg/runner/aws/util"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	"github.com/spf13/cobra"
@@ -15,6 +18,7 @@ func consoleLogCmd() *cobra.Command {
 	var (
 		tagName string
 		region  string
+		wait    bool
 	)
 
 	cmd := &cobra.Command{
@@ -28,35 +32,49 @@ func consoleLogCmd() *cobra.Command {
 				log.Fatalf("failed to load AWS config: %v", err)
 			}
 
-			inst, err := util.GetInstanceByTag(ctx, client, tagName)
+			inst, err := awsutil.GetInstanceByTag(ctx, client, tagName)
 			if err != nil {
 				log.Fatalf("Failed to get instances in %s with tag %s: %v", region, tagName, err)
 			}
 
 			input := &ec2.GetConsoleOutputInput{
 				InstanceId: inst.InstanceId,
+				Latest:     utils.ToPtr(true),
 			}
 
-			output, err := client.GetConsoleOutput(ctx, input)
-			if err != nil {
-				log.Fatalf("failed to get console output: %v", err)
+			var decoded []byte
+			for {
+				output, err := client.GetConsoleOutput(ctx, input)
+				if err != nil {
+					log.Fatalf("failed to get console output: %v", err)
+				}
+				if output.Output == nil {
+					log.Fatalf("no console output available for instance: %s", *inst.InstanceId)
+				}
+				decoded, err = base64.StdEncoding.DecodeString(*output.Output)
+				if err != nil {
+					log.Fatalf("base64 decoding for console log of %s failed: %v", *inst.InstanceId, err)
+				}
+				if strings.TrimSpace(string(decoded)) != "" {
+					break
+				}
+				if !wait {
+					log.Fatalf("console output is empty for instance: %s", *inst.InstanceId)
+				}
+				select {
+				case <-ctx.Done():
+					wait = false
+				case <-time.After(500 * time.Millisecond):
+					continue
+				}
 			}
-
-			if output.Output == nil {
-				log.Fatalf("no console output available for instance: %s", *inst.InstanceId)
-			}
-
-			decoded, err := base64.StdEncoding.DecodeString(*output.Output)
-			if err != nil {
-				log.Fatalf("base64 decoding for console log of %s failed: %v", *inst.InstanceId, err)
-			}
-
 			fmt.Println(string(decoded))
 		},
 	}
 
 	cmd.Flags().StringVar(&tagName, "tag", "", "Tag name of the instance (required)")
 	cmd.Flags().StringVar(&region, "region", "", "AWS region (required)")
+	cmd.Flags().BoolVar(&wait, "wait", false, "wait for  console logs  to become available")
 	cmd.MarkFlagRequired("tag")
 	cmd.MarkFlagRequired("region")
 
