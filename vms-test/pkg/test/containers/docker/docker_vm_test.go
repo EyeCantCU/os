7a8b378959421b623d21327ddacc5c139dd55389
Zach Marano <zach.marano@chainguard.dev>
2025-06-06 13:54:17 -0700

    Fix a couple test bugs (#401)
    - Typo on disk_type
    - Run docker with -q to suppress the pull output.

--- vms-test/pkg/test/containers/docker/docker_vm_test.go
+++ vms-test/pkg/test/containers/docker/docker_vm_test.go
@@ -15,7 +15,7 @@ func TestDockerPullAndRun(t *testing.T) {
 	var cmd *exec.Cmd
 
 	// See if we can pull a wolfi-base container image and run it
-	cmd = exec.CommandContext(ctx, "docker", "run", "cgr.dev/chainguard/wolfi-base:latest", "echo", "Hello Wolfi")
+	cmd = exec.CommandContext(ctx, "docker", "run", "-q", "cgr.dev/chainguard/wolfi-base:latest", "echo", "Hello Wolfi")
 
 	out, err := cmd.CombinedOutput()
 	if err != nil {
