7e5478720a34ab7364f7750186869ac7950a54d2
Markus Boehme <markus.boehme@chainguard.dev>
2025-08-28 14:54:34 +0200

    vms-test: ssmsendcommand: reformat launcher script
    No functional change intended. Add some vertical space between steps and
    break overly long lines.
    
    Signed-off-by: Markus Boehme <markus.boehme@chainguard.dev>

--- vms-test/pkg/test/aws/ssmsendcommand/ssmsendcommand_launch.sh
+++ vms-test/pkg/test/aws/ssmsendcommand/ssmsendcommand_launch.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 set -o pipefail
 WAIT_SSH_TIMEOUT=${WAIT_SSH_TIMEOUT:-5m}
 
@@ -54,20 +55,36 @@ main() {
     local region="" tag="" instance_id=""
     region=$(findarg --region "$@") || failrc "can't find --region"
     tag=$(findarg --tag "$@") || failrc "can't find --tag"
+
     # Launch instance
     vr "$RUNNER" launch "$@" || failrc "can't launch instance"
+
+    # Wait for instance readiness
     timeout "timeout waiting for ssm agent node to appear"
     trap cancel_timeout exit
-    # Wait for instance readiness
-    vr "$RUNNER" wait-for-ssh --timeout "$WAIT_SSH_TIMEOUT" --region "$region" --tag "$tag" || failrc "ssh never became available"
+    vr "$RUNNER" wait-for-ssh --timeout "$WAIT_SSH_TIMEOUT" --region "$region" --tag "$tag" \
+        || failrc "ssh never became available"
+
     # Wait for SSM agent registration
     log "waiting for ssm agent to register with control plane"
-    instance_id=$(aws ec2 describe-instances --region "$region" --filters "Name=tag:Name,Values=$tag" | jq -re '.Reservations[0].Instances[0].InstanceId') || failrc "can't find instance ID"
+    instance_id=$(
+        aws ec2 describe-instances --region "$region" --filters "Name=tag:Name,Values=$tag" \
+            | jq -re '.Reservations[0].Instances[0].InstanceId'
+    ) || failrc "can't find instance ID"
+
     local nodes="0"
     while [ "$nodes" == "0" ]; do
-        nodes=$(aws ssm list-nodes --region "$region" --filters "Key=InstanceId,Values=$instance_id" "Key=InstanceStatus,Values=Active" "Key=ManagedStatus,Values=Managed" | jq -re '.Nodes | length') || failrc "can't list ssm agent nodes"
+        nodes=$(
+            aws ssm list-nodes \
+                    --region "$region" \
+                    --filters "Key=InstanceId,Values=$instance_id" \
+                              "Key=InstanceStatus,Values=Active" \
+                              "Key=ManagedStatus,Values=Managed" \
+                | jq -re '.Nodes | length'
+        ) || failrc "can't list ssm agent nodes"
         sleep 1
     done
+
     # Send command
     vr aws ssm send-command --region "$region" \
         --document-name "AWS-RunShellScript" \
