d56114711eac56222e39f40bf0e5e60f939dc94b
Amelia Crate <amelia.crate@chainguard.dev>
2025-09-05 16:29:48 -0500

    vms-test containers/docker: retry container pull

--- vms-test/pkg/test/containers/docker/docker_vm_test.go
+++ vms-test/pkg/test/containers/docker/docker_vm_test.go
@@ -6,21 +6,40 @@ import (
 	"os/exec"
 	"strings"
 	"testing"
+	"time"
 
 	"chainguard.dev/wolfi-vm/vm-test/pkg/vmtest"
 )
 
+const (
+	pullAttempts      = 3
+	pullBackoffFactor = 1
+)
+
 func TestDockerPullAndRun(t *testing.T) {
 	ctx := vmtest.Context(t)
-	var cmd *exec.Cmd
+	var pullCmd *exec.Cmd
+	var err error
+	var out []byte
+
+	for i := 0; i < pullAttempts; i++ {
+		pullCmd = exec.CommandContext(ctx, "docker", "pull", "-q", "cgr.dev/chainguard/wolfi-base:latest")
+		out, err = pullCmd.CombinedOutput()
+		if err == nil {
+			break
+		}
+		time.Sleep(time.Duration(i*pullBackoffFactor) * time.Second)
+	}
+	if err != nil {
+		t.Fatalf("exec.CommandContext(ctx, %s) = err %v after %d attempts, want nil\noutput:%s", pullCmd.String(), err, pullAttempts, string(out))
+	}
 
 	// See if we can pull a wolfi-base container image and run it
-	cmd = exec.CommandContext(ctx, "docker", "run", "-q", "cgr.dev/chainguard/wolfi-base:latest", "echo", "Hello Wolfi")
+	runCmd := exec.CommandContext(ctx, "docker", "run", "-q", "cgr.dev/chainguard/wolfi-base:latest", "echo", "Hello Wolfi")
 
-	out, err := cmd.CombinedOutput()
+	out, err = runCmd.CombinedOutput()
 	if err != nil {
-		t.Errorf("exec.CommandContext(ctx, %s) = err %v, want nil\noutput:%s", cmd.String(), err, string(out))
-		return
+		t.Fatalf("exec.CommandContext(ctx, %s) = err %v, want nil\noutput:%s", runCmd.String(), err, string(out))
 	}
 
 	// We would expect to get "Hello Wolfi"
