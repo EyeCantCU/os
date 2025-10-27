765abb91344636bed008ffa11e941a5ee19cad0e
Luca Di Maio <luca.dimaio1@gmail.com>
2025-10-27 13:53:32 +0100

    fix(vms-test): check for default.target (#983)
    Move the check from graphical to default.
    Not always the graphical target is available, for example on hardened
    images it is disabled as a requirement:
    
    
    https://github.com/chainguard-dev/enterprise-packages/blob/cc89236a79ffad60b390f9df9fc2fa03550c5994/harden-vm/apply-hardening#L452
    
    Using default target allows us to still check for the main target, being
    it either graphical or just multi-user
    
    Signed-off-by: Luca Di Maio <luca.dimaio1@gmail.com>

--- vms-test/pkg/test/core/systemd/systemd_vm_test.go
+++ vms-test/pkg/test/core/systemd/systemd_vm_test.go
@@ -112,10 +112,10 @@ func TestCollectLogs(t *testing.T) {
 	myCtx, cancel := context.WithTimeout(ctx, timeout)
 	defer cancel()
 
-	// wait for graphical.target
-	st, err := systemd.GetServiceStartMonotonic(myCtx, "graphical.target")
+	// wait for default.target
+	st, err := systemd.GetServiceStartMonotonic(myCtx, "default.target")
 	if err != nil {
-		t.Errorf("fail waiting for graphical.target: %v\n", err)
+		t.Errorf("fail waiting for default.target: %v\n", err)
 	}
 
 	artifacts.Log(t, metrics.MultiUserTarget, fmt.Sprintf("%f", st.Seconds()), nil)
